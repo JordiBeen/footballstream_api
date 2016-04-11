ee20a4017a811950863547236c26bd9b2aa5c969
jordibeen <jordibeen1996@gmail.com>
2016-04-11 05:42:10 -0400

    Updated entrypoints

--- footballstream_api/handlers/general.py
+++ footballstream_api/handlers/general.py
@@ -2,8 +2,6 @@ import logging
 
 from pyramid.view import view_config
 
-from ..models.user import get_user, list_users
-
 log = logging.getLogger(__name__)
 
 
@@ -12,7 +10,33 @@ log = logging.getLogger(__name__)
 def general_entry_points(request):
     return {
         'entry_points': {
-            'users.list': '/api/v1/users',
-            'users.get': '/api/v1/users/{user_id}'
+            'users': {
+                'list': '/api/v1/users',
+                'get': '/api/v1/users/{user_id}'
+            },
+            'competitions': {
+                'list': '/api/v1/competitions',
+                'get': '/api/v1/competitions/{competition_id}'
+            },
+            'standings': {
+                'list': '/api/v1/standings',
+                'get': '/api/v1/standings/{standing_id}'
+            },
+            'matches': {
+                'list': '/api/v1/matches',
+                'get': '/api/v1/matches/{match_id}'
+            },
+            'commentaries': {
+                'list': '/api/v1/commentaries',
+                'get': '/api/v1/commentaries/{commentary_id}'
+            },
+            'teams': {
+                'list': '/api/v1/teams',
+                'get': '/api/v1/teams/{team_id}'
+            },
+            'players': {
+                'list': '/api/v1/players',
+                'get': '/api/v1/players/{player_id}'
+            }
         }
     }
